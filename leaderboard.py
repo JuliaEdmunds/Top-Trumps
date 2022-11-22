babf69db6a4e9061fe26807f23248b231ef5d947
JuliaEdmunds <julia.edmunds112@gmail.com>
2022-11-23 03:32:10 +0800

    added a comment

--- leaderboard.py
+++ leaderboard.py
@@ -1,5 +1,5 @@
 # Here we need to write to a csv file the name and score of the winner if it's within the top 5 scores
 # Create following functions:
 #   1) Check if the score is eligible to show in the leaderboard
-#   2) Write current name, score and deck (either Pokemon or NBA)  to a leaderboard (sort by score) 
+#   2) Write current name, score and deck (either Pokemon or NBA) to a leaderboard (sort by score) - comment update
 print("hello")
