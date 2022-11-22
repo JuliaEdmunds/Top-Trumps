60b28ddd83de3c4a4901895bc14a4e332dc0b6eb
JuliaEdmunds <julia.edmunds112@gmail.com>
2022-11-23 03:08:10 +0800

    updated leaderboard instruction part

--- leaderboard.py
+++ leaderboard.py
@@ -1,5 +1,4 @@
-# Here we need to write to a csv file the name and score of the winner if it's within the top 5 scores (Score: number
-# of rounds won)
+# Here we need to write to a csv file the name and score of the winner if it's within the top 5 scores
 # Create following functions:
 #   1) Check if the score is eligible to show in the leaderboard
-#   2) Write current name & score to a leaderboard (sort by score)
+#   2) Write current name, score and deck (either Pokemon or NBA)  to a leaderboard (sort by score) 
