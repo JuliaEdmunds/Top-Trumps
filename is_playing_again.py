e01c2992d37c4d8e6a703be7085f8a29ba4ea121
JuliaEdmunds <julia.edmunds112@gmail.com>
2022-11-25 16:04:17 +0100

    Implemented play again
    I have added an option to play the game again (updated main and added new is_playing_again file)

---
+++ is_playing_again.py
@@ -0,0 +1,14 @@
+import helpers
+
+
+def keep_playing():
+    is_still_playing = input("Do you want to try again? (yes/no) ").lower().strip()
+    while is_still_playing != "yes" and is_still_playing != "no":
+        is_still_playing = input("Pleas answer yes or no? ").lower().strip()
+
+    if is_still_playing == "yes":
+        helpers.clear()
+        return True
+    else:
+        print("Thanks for playing! Bye...")
+        return False
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import get_card_deck
 import play_round
 import time
 import helpers
+import is_playing_again
 
 
 # Starting the "real" code here, for now I have selected these parameters, but we can discuss and change
@@ -25,37 +26,42 @@ Player with higher stats wins a round. You will play total of {num_of_rounds} ro
 Each round you earn points: for win {win_points} points, for draw {draw_points} points and for a lost round {lost_points} point.\n""")
 time.sleep(5)
 
-# Get the deck number from the player
-chosen_deck = get_card_deck.choose_deck()
+playing = True
+while playing:
+    # Get the deck number from the player
+    chosen_deck = get_card_deck.choose_deck()
 
-player_score = 0
-player_wins = 0
-computer_wins = 0
+    player_score = 0
+    player_wins = 0
+    computer_wins = 0
 
-for round_num in range(num_of_rounds):
-    current_round = round_num + 1
-    # Play a round tracking round number & chosen deck for correct cards
-    current_round_result = play_round.play_round(current_round, chosen_deck)
-    if current_round_result == E_scores.Score.win:
-        player_wins += 1
-    elif current_round_result == E_scores.Score.lost:
-        computer_wins += 1
+    for round_num in range(num_of_rounds):
+        current_round = round_num + 1
+        # Play a round tracking round number & chosen deck for correct cards
+        current_round_result = play_round.play_round(current_round, chosen_deck)
+        if current_round_result == E_scores.Score.win:
+            player_wins += 1
+        elif current_round_result == E_scores.Score.lost:
+            computer_wins += 1
 
-    to_print = current_round_result.name
-    player_score += current_round_result.value
+        to_print = current_round_result.name
+        player_score += current_round_result.value
 
-    print(f"\nYou {to_print} the round. Current results after {current_round} round/s:"
-          f"\nPlayer wins: {player_wins}, points: {player_score}"
-          f"\nOpponent wins: {computer_wins}\n")
-    time.sleep(7)
-    # Clear the screen for the next round
-    helpers.clear()
+        print(f"\nYou {to_print} the round. Current results after {current_round} round/s:"
+              f"\nPlayer wins: {player_wins}, points: {player_score}"
+              f"\nOpponent wins: {computer_wins}\n")
+        time.sleep(7)
+        # Clear the screen for the next round
+        helpers.clear()
 
-if player_wins > computer_wins:
-    print(f"Congrats. You won the game with {player_wins} wins (opponent won only {computer_wins} rounds) and "
-          f"a total of {player_score} points")
-elif player_wins < computer_wins:
-    print(f"Sorry this time you lost the game... You won {player_wins} rounds while opponent won {computer_wins}. "
-          f"Your total score is {player_score} points")
-else:
-    print(f"It's a draw. Both you and your opponent won {player_wins} rounds. Your total score is {player_score} points")
+    if player_wins > computer_wins:
+        print(f"Congrats. You won the game with {player_wins} wins (opponent won only {computer_wins} rounds) and "
+              f"a total of {player_score} points")
+    elif player_wins < computer_wins:
+        print(f"Sorry this time you lost the game... You won {player_wins} rounds while opponent won {computer_wins}. "
+              f"Your total score is {player_score} points")
+    else:
+        print(f"It's a draw. Both you and your opponent won {player_wins} rounds. Your total score is {player_score} "
+              f"points")
+
+    playing = is_playing_again.keep_playing()
